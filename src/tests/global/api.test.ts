c07234d708c60a2578fe824d675ff558b5647e86
Fool's Mate <mail@foolsmate.de>
2020-07-05 19:15:34 +0200

    fix(test): correct typos

--- src/tests/global/api.test.ts
+++ src/tests/global/api.test.ts
@@ -12,13 +12,13 @@ afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 describe('API', () => {
-  it('should handle the argument as a request parameters and return data', async () => {
+  it('should handle the argument as a request parameter and return data', async () => {
     const user = await fetchUser('fools-mate');
 
     expect(user).toStrictEqual({ login: 'fools-mate', name: "Fool's Mate" });
   });
 
-  it('should throw an error when connection fails', async () => {
+  it('should throw an error when the connection fails', async () => {
     server.use(
       // Setup request handler to response with status: 404 - Not Found
       rest.get('https://api.github.com/users/:user', (req, res, ctx) => {
