78fde5059214f214200cdbfd3bc01e5193304cb5
Fool's Mate <mail@foolsmate.de>
2020-07-06 18:06:11 +0200

    feat(component): implement useSearchUser hook

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
-import { useDebounceInput, useFetchUser } from '../global/hooks';
+import { useDebounceInput, useSearchUser } from '../global/hooks';
 
 const InputField = styled.input`
   padding: 0.31rem 0.44rem;
@@ -9,18 +9,24 @@ const InputField = styled.input`
   border-radius: 4px;
 `;
 
+const ProfilePicture = styled.img`
+  width: 100px;
+  height: auto;
+  border-radius: 100%;
+`;
+
 const SearchUser: React.FC = () => {
   const [searchInput, setSearchInput] = useState('');
   // On all input changes the debounce hook is called this prevents API calls at every keypress
   const debouncedSearchInput = useDebounceInput(searchInput, 1000);
   // After debouncing, the search query is updated and the request is sent to the GitHub API
   const {
-    userData,
+    searchData,
     isError,
     isLoading,
     isValidInput,
     validationErrorMsg,
-  } = useFetchUser(debouncedSearchInput ? debouncedSearchInput : null, {
+  } = useSearchUser(debouncedSearchInput ? debouncedSearchInput : null, {
     // Prevents call to API after page is focused or user is reconnected and also retries on errors
     // This settings are good practice for development
     revalidateOnFocus: false,
@@ -41,19 +47,22 @@ const SearchUser: React.FC = () => {
       {searchInput && isLoading && isValidInput && <p>Searching..</p>}
       {isError && isValidInput && (
         <>
-          <p>No one with this username was found.</p>
-          <p>Do you want to register this username? </p>
-          <a href="https://github.com/join" rel="noreferrer" target="_blank">
-            Join GitHub
-          </a>
+          <p>No one was found.</p>
         </>
       )}
       {!isValidInput && <p>{validationErrorMsg}</p>}
       {!isLoading && !isError && (
         <>
-          <h1>{userData.name}</h1>
-          <p>{userData.login}</p>
-          <p>{userData.bio}</p>
+          {searchData.items.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>
+                  <ProfilePicture src={user.avatar_url} alt="Profile picture" />
+                  <a href={`/users/${user.login}`}>{user.login}</a>
+                </p>
+              </div>
+            );
+          })}
         </>
       )}
     </div>
