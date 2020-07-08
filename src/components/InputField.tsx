383e3158a251c6c86ab10de6d94dc03d4287849c
Fool's Mate <mail@foolsmate.de>
2020-07-08 18:00:27 +0200

    refactor(ui): increase padding

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const InputField = styled.input`
   width: 100%;
-  padding: 0.31rem 0.44rem;
+  padding: 0.28rem 0.56rem;
   font-size: 1.22rem;
   border: 1px solid lightgrey;
   border-radius: 4px;
