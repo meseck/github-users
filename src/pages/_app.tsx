c4c1f9978e5113b37a46485c96978ce40e5d1669
Fool's Mate <mail@foolsmate.de>
2020-07-08 09:41:55 +0200

    feat(ui): setup global normalize, theme & styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import 'normalize.css/normalize.css';
+
+import '../styles/global.css';
+import theme from '../styles/theme';
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    primary: 'lightblue',
+    secondary: 'blue',
+  },
+};
+
+export default theme;
